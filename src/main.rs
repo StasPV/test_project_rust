331082f0783d000838249d87831bb52c57522966
staspv <staspv2008@gmail.com>
2024-10-29 23:05:20 +0300

    мелкие изменения

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::process;
 
 fn main() {
+    println!("полная жопа");
     if let Err(e) =  test_project_rust::run_tests(){
         eprintln!("Application error: {e}");
         process::exit(1);        
