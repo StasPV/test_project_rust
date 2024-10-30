e5c8cd2ebeb3c903bb170064a8ff383e6dab58bf
staspv <staspv2008@gmail.com>
2024-10-31 00:03:47 +0300

    мелкие настройки среды разработки

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::process;
 
 fn main() {
-    println!("полная жопа");
     if let Err(e) =  test_project_rust::run_tests(){
         eprintln!("Application error: {e}");
         process::exit(1);        
