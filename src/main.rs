71a869b6fde79d02da5611f473a845698413bb9e
staspv <staspv2008@gmail.com>
2024-11-06 07:53:43 +0300

    пройденные уроки на 05.11

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::process;
+use test_project_rust::Tests;
+use test_project_rust as tpr;
 
 fn main() {
-    if let Err(e) =  test_project_rust::run_tests(){
+    if let Err(e) =  tpr::run_tests(Tests::Post){
         eprintln!("Application error: {e}");
         process::exit(1);        
     }
