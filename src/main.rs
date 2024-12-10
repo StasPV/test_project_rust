ebfe5697bc693448a38504cebebdafd1003536bf
staspv <staspv2008@gmail.com>
2024-12-10 18:58:02 +0300

    Доработка блока многопоточности: реализация передачи данных между каналами.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 use test_project_rust as tpr;
 
 fn main() {
-    if let Err(e) =  tpr::run_test::<tpr::fview::Fview>(){
+    if let Err(e) =  tpr::run_test::<tpr::rustthread::RustThread>(){
         eprintln!("Application error: {e}");
         process::exit(1)
     }
--- src/rustthread.rs
+++ src/rustthread.rs
@@ -1,42 +1,41 @@
-use std::sync::{Mutex, mpsc, Arc};
+use crate::BaseObject;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use crate::BaseObject;
 
-pub struct ThreadTest;
-impl BaseObject for ThreadTest{
-    fn new()->Self {
-        ThreadTest{}
+pub struct RustThread;
+impl BaseObject for RustThread {
+    fn new() -> Self {
+        RustThread {}
     }
 
     fn run(&self) {
         println!("Урок по многопоточности");
-        cannels();
-        
-        println!("Mutex");
-        mutex();
+        two_chanels();
     }
 }
 
-fn mutex(){
+#[allow(dead_code)]
+fn mutex() {
     let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
-    for _ in 0..10{
+    for _ in 0..10 {
         let counter = Arc::clone(&counter);
-        let handle = thread::spawn(move||{
+        let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
         });
         handles.push(handle);
     }
 
-    for handle in handles{
+    for handle in handles {
         handle.join().unwrap();
     }
     println!("result: {}", *counter.lock().unwrap());
 }
 
+#[allow(dead_code)]
 fn cannels() {
     let (tx, rx) = mpsc::channel();
     let tx1 = tx.clone();
@@ -69,4 +68,66 @@ fn cannels() {
     for recived in rx {
         println!("Получено сообщение: {recived}");
     }
+}
+
+#[derive(Debug)]
+enum MessageType {
+    Ping,
+    Pong,
+    Pang,
+}
+
+fn two_chanels() {
+    let mut handles = vec![];
+    let (request_tx, request_rx) = mpsc::channel::<MessageType>();
+    let (response_tx, response_rx) = mpsc::channel::<MessageType>();
+    let mut count: u32 = 1;
+
+    request_tx.send(MessageType::Ping).unwrap();
+    let handle = thread::spawn(move || loop {
+        let message = match response_rx.recv() {
+            Ok(value) => value,
+            Err(_) => return,
+        };
+        println!("Канал 1. Получено сообщение: {:?} {}", message, count);
+        match message {
+            MessageType::Pong => match request_tx.send(MessageType::Ping){
+                Err(_)=> return,
+                _=>(),
+            }
+            // MessageType::Pang => break,
+            _ => (),
+        }
+
+        count += 1;
+        if count > 10 {
+            request_tx.send(MessageType::Pang).unwrap();
+            return;
+        }
+    });
+    handles.push(handle);
+
+    let handle = thread::spawn(move || loop {
+        let message = match request_rx.recv() {
+            Ok(value) => value,
+            Err(_) => return,
+        };
+        println!("Канал 2. Получено сообщение: {:?}", message);
+        match message {
+            MessageType::Ping => {
+                match response_tx.send(MessageType::Pong){
+                    Err(_)=> return,
+                    _=>(),
+                }
+            },
+            MessageType::Pang => return,
+            _ => (),
+        }
+    });
+    handles.push(handle);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Игра завершена!")
 }
