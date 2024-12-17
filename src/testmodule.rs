038feb09e89dc7514eb531f3867367f69d37eb4b
staspv <staspv2008@gmail.com>
2024-12-17 09:15:18 +0300

    Форматирование сообщения пустого типа

--- src/testmodule.rs
+++ src/testmodule.rs
@@ -2,6 +2,7 @@
 pub trait BaseObject<RHS=Self>{
     fn new()->Self;
     fn run(&self){
-        dbg!("Пустая реализация типа");
+        println!
+        ("Пустая реализация типа {}", std::any::type_name_of_val(self));
     }
  }
