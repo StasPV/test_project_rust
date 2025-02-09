70f492dffb82000f05d9e0f645661cd4bd2815a2
staspv <staspv2008@gmail.com>
2025-02-09 19:06:37 +0300

    мелкие доработки

--- examples/lab02.rs
+++ examples/lab02.rs
@@ -2,20 +2,23 @@
 struct Gopa{
     name: String,
     age: u32,
+    len: u32,
 }
 
 fn main(){
     let gopa = Gopa{
         name: String::from("Gopa new form"),
         age: 50,
+        len: 1240
     };
     let arr = get_memory(&gopa);
     let len = arr.len();
 
     let new_str:[u8; 4] = [71,111,112,97];
-    println!("{:?} - {:?}", &(gopa.name.as_bytes()), String::from_utf8_lossy(&new_str));
+    let new_str2 = [0, 3, 208, 42, 139, 1, 0, 0].as_ptr();
+    println!("{:?} - {:?} - {:?}", &(gopa.name.as_bytes()), String::from_utf8_lossy(&new_str),String::from(&new_str2));
     // unsafe{stc = std::mem::transmute::<&[u8], Gopa>(arr)};
-    println!("{:?}, {:?}", arr, len);
+    println!("{:?}, {:?}, {:08x}", arr, len, gopa.len);
 }
 
 fn get_memory<'a, T>(input: &'a T)->&'a [u8]{
